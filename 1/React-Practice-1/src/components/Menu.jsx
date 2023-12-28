2d8b1d1a33852ca5e3c90352a8cf7245be699b67
Pranay Desai <pdesai22@stevens.edu>
2023-12-28 12:53:39 -0500

    React-Practice-1 JSX,Components,props

---
+++ 1/React-Practice-1/src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import Pizza from "./Pizza"
+export default function Menu() {
+  return (
+      <div>
+          <Pizza pizzaName="Pepperoni" />
+          <Pizza pizzaName="Margherita" />
+          <Pizza pizzaName="Hawaiian" />
+    </div>
+  )
+}
---
+++ 1/React-Practice-1/src/components/Pizza.jsx
@@ -0,0 +1,8 @@
+
+export default function Pizza(props)
+{
+    const pizzaName = props.pizzaName
+  return (
+      <div>{pizzaName}</div>
+  )
+}
